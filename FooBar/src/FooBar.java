7b628676c3f85dc50ed765b1a599f4d1cdbc90f7
acorley094 <ahrikcorley@gmail.com>
2021-06-10 16:44:06 -0400

    I got it

--- FooBar/src/FooBar.java
+++ FooBar/src/FooBar.java
@@ -1,16 +1,46 @@
+import java.util.Scanner;
 
 public class FooBar {
 	public static void main(String[] args) {
 		int x = 1;
 		while (x <= 100) {
-			System.out.println(x);
-			if (x % 3 == 0) {
+			if((x % 3 ==0) & (x % 5==0)) {
+				System.out.println("FooBar");
+			}
+			else if (x % 3 == 0) {
 				System.out.println("Bar");
-			};
-			if (x % 5 == 0) {
+			}
+			else if (x % 5 == 0) {
 				System.out.println("Foo");
-			};
-			x++;
-		}
+			}
+			else{
+				System.out.println(x);
+			}
+			x++;}
+		
+		System.out.println("Give me a number to FooBar: ");
+		Scanner userInput = new Scanner(System.in);
+		fooBar(userInput.nextInt());
 	}
+	
+	
+	public static void fooBar(int x) {
+		int i = 1;
+		while (i <= x) {
+			if((i % 3 ==0) & (i % 5==0)) {
+				System.out.println("FooBar");
+			}
+			else if (i % 3 == 0) {
+				System.out.println("Bar");
+			}
+			else if (i % 5 == 0) {
+				System.out.println("Foo");
+			}
+			else{
+				System.out.println(i);
+			}
+			i++;
+	}
+}
+
 }
---
+++ bin/Lab-ConditionalStatements/bin/README.md
@@ -0,0 +1,17 @@
+# Shellebration
+
+Dakarai: 
+-Display Simple Datat Types
+-Write Simple Math Statements
+
+Kevin:
+-Casting Datat Types
+-String Concatenation
+
+Cynthia:
+-Conditional Blocs
+-Variant Conditional Blocks
+
+Ahrik:
+-Nested Conditional Blocks
+-Switch/Fall-through Logic
